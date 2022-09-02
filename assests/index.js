0cede5eef8906c553fc80cebec5ae3e9f253be96
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-09-02 00:46:03 -0400

    add a fetch

--- assests/index.js
+++ assests/index.js
@@ -1,20 +1,41 @@
 const apiHost = " http://localhost:3000";
 
-function addBook(evt){
+function addBook(evt) {
     evt.preventDefault();
-    const bookName =document.getElementById('name');
-    const bookDescription= document.getElementById('description');
-    const author= document.getElementById('author');
+
+
+    const book={
+        bookName:document.getElementById('name').value,
+        bookDescription:document.getElementById('description').value,
+        author:document.getElementById('author').value,
+    }
+        // const bookName =document.getElementById('name');
+        // const bookDescription= document.getElementById('description');
+        // const author= document.getElementById('author');
 
     const submitbutton = document.getElementById('bookAddbutton');
     submitbutton.disable = true;
     submitbutton.innerHTML="adding a book..";
 
-    fetch()
-
+    fetch(`${apiHost}/books`,{
+        method:"POST",
+        body:JSON.stringify(book),
+        headers:{
+            'content=Type':'application/json',
+        }
+    })
+    .then(response=>{
+        console.log(response);
+    });
+    
+    
 }
 
 document.addEventListener("DOMContentLoaded",()=>{
     console.log();
+
+    const addBookForm =document.getElementById('add-book-form');
+    addBookForm.addEventListener('submit', addBook);
+    
 
 });
