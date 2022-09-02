eddef32461b5e2b5577c26c0b8556e460aae139b
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-09-01 20:31:19 -0400

    add my object data books in my JSON server

--- assests/index.js
+++ assests/index.js
@@ -1,8 +1,16 @@
+const apiHost = " http://localhost:3000";
+
 function addBook(evt){
     evt.preventDefault();
     const bookName =document.getElementById('name');
     const bookDescription= document.getElementById('description');
     const author= document.getElementById('author');
+
+    const submitbutton = document.getElementById('bookAddbutton');
+    submitbutton.disable = true;
+    submitbutton.innerHTML="adding a book..";
+
+    fetch()
 
 }
 
